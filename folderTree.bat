ed70f9079511f5abc54f57f65809758c7dc2d41b
hausmanne <54555072+hausmanne@users.noreply.github.com>
2020-08-02 21:56:31 +0200

    batch folder created

---
+++ folderTree.bat
@@ -0,0 +1,24 @@
+mkdir assets
+
+cd assets
+
+mkdir css
+mkdir font
+mkdir images
+mkdir sass
+
+cd sass
+
+mkdir base
+mkdir utils
+mkdir layouts
+mkdir components
+mkdir pages
+mkdir themes
+mkdir vendors
+echo. 2>main.scss
+
+cd ..
+cd css
+
+echo. 2>application.css
